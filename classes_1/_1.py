be3618f8548f8485901a55871c8df59b39b4cd16
notforyou! <notforyou.creator@gmail.com>
2025-02-19 20:39:41 +0300

    Update _1.py

--- classes_1/_1.py
+++ classes_1/_1.py
@@ -3,20 +3,28 @@ class Pet:
 		self.name = name
 		self.animal_type = animal_type
 		self.age = age
+		
 	def __str__():
 		return "Pet"
+		
 	def set_name(self, name):
 		self.name = name
+		
 	def set_animal_type(self, animal_type):
 		self.animal_type = animal_type
+		
 	def set_age(self, age):
 		self.age = age
+		
 	def get_name(self):
 		return self.name
+		
 	def get_animal_type(self):
 		return self.animal_type
+		
 	def get_age(self):
 		return self.age
+		
 		
 your_pet = Pet(input("Имя"), input("Тип животного"), int(input("Возраст")))
 print(your_pet.get_name())
